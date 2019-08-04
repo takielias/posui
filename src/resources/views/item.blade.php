58c9450e150f2556a137330081f125ac7bfac7b7
takielias <takiahmadelias@gmail.com>
2019-08-04 17:09:20 +0600

    Style Updated

--- src/resources/views/item.blade.php
+++ src/resources/views/item.blade.php
@@ -1,35 +1,35 @@
 <tr id="tr-{{$item->id}}">
-    <td width="3%">1</td>
-    <td width="40%">
+    <td>1</td>
+    <td>
         {{$item->product_name}}
         <div class="help-block with-errors"></div>
     </td>
-    <td width="10%">
+    <td>
         <div class="input-group col-md-12">
             <input type="number" id="item-quantity-{{$item->id}}" name="item_quantity[0]" value="1" class="form-control"
                    onchange="totalCount()">
         </div>
     </td>
-    <td width="12%">
+    <td>
         <div class="input-group col-md-12">
             <input type="text" id="item-price-{{$item->id}}" name="item_price[0]" value="{{$item->product_price}}"
                    class="form-control">
         </div>
     </td>
-    <td width="10%">
+    <td>
         <div class="input-group col-md-10">
             <input type="text" id="item-discount-{{$item->id}}" name="discount[0]" value="0.00" class="form-control"
                    readonly="">
         </div>
     </td>
-    <td width="17%">
+    <td>
         <div class="input-group col-md-10">
             <input type="text" id="item-total-{{$item->id}}" name="item_total[0]" value="{{$item->product_price}}"
                    class="form-control"
                    readonly="">
         </div>
     </td>
-    <td width="8%">
+    <td>
         <a href="javascript:;" class="btn btn-danger btn-remove-row">
             X
         </a>
@@ -42,6 +42,7 @@
         $("#ps").val('');
         totalCount();
     });
+
     function totalCount() {
         var priceElement = $('input[name*="item_price"]');
         var quantityElement = $('input[name*="item_quantity"]');
